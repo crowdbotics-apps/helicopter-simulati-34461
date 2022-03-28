ed2deaad11d68505bf123bb40be796f600c50614
Crowdbotics <team@crowdbotics.com>
2022-03-28 04:06:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "helicopter_simulati_34461",
+  url: "https://helicopter_simulati_34461.botics.co",
+  api: "https://helicopter_simulati_34461.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
